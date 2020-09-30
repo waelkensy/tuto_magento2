5c2851ac280bfa39089fd1b061dd6f8911df1d6d
Yannick Waelkens <yannick.waelkens@cgi.com>
2020-09-30 09:33:57 +0200

    fix error when di:compile

--- Model/PointOfSales/DataProvider.php
+++ Model/PointOfSales/DataProvider.php
@@ -20,9 +20,9 @@ class DataProvider extends AbstractDataProvider
     /**
      * DataProvider constructor.
      *
-     * @param                   $name
-     * @param                   $primaryFieldName
-     * @param                   $requestFieldName
+     * @param string            $name
+     * @param string            $primaryFieldName
+     * @param string            $requestFieldName
      * @param array             $meta
      * @param array             $data
      * @param CollectionFactory $pointOfSalesCollectionFactory
